f8c75442e6672ddb345197004815209c28d33f5a
Nikhil Srivastava <nikhilsrivastavaone@gmail.com>
2022-06-28 22:54:38 +0530

    first commit

---
+++ models/temp.js
@@ -0,0 +1,23 @@
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId('62b7220685dd82419a4dee28'),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: '$rating',
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
